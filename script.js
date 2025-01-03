2d0a6abb5be5ff82dbd24dccbfdf7808331fe3c8
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2025-01-03 17:18:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+let timerdisplay=document.getElementById("timer");
+let currentDate=new Date();
+
+setInterval
+{
+	function()
+		{
+			currentDate=new Date();
+			timerdisplay.innerHTML=currentDate.toLocaleString();
+			
+        },1000
+}
